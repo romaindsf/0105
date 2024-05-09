a8877a3488826ec01b6ff4c84ea0df6c3b62a5f4
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-05-09 19:00:11 +0200

    setState activeSection has to be defined

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,40 @@
+'use client'
 import styles from '../styles/_page.module.scss'
 import PresentationSection from '@/components/presentationSection/PresentationSection'
 import CompetenceSection from '@/components/competenceSection/CompetenceSection'
 import RealisationsSection from '@/components/realisationsSection/RealisationsSection'
 import ContactSection from '@/components/contactSection/ContactSection'
 import CvSection from '@/components/cvSection/CvSection'
+import { useState } from 'react'
 
 export default function Home() {
+  const [state, setState] = useState('__default')
+
+  const activeSection = () => {
+    setState(state === '__default' && 'active')
+  }
+
   return (
     <div className={styles.grid}>
       <PresentationSection
         className={`${styles.grid__item} ${styles.presentationSection__default}`}
+        onClick={activeSection}
       />
       <CompetenceSection
         className={`${styles.grid__item} ${styles.competenceSection__default}`}
+        onClick={activeSection}
       />
       <RealisationsSection
         className={`${styles.grid__item} ${styles.realisationsSection__default}`}
+        onClick={activeSection}
       />
       <ContactSection
         className={`${styles.grid__item} ${styles.contactSection__default}`}
+        onClick={activeSection}
       />
       <CvSection
         className={`${styles.grid__item} ${styles.cvSection__default}`}
+        onClick={activeSection}
       />
     </div>
   )
