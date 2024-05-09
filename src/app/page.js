d9aacd6aa0dd36bfd25905bb2f3e7b37e9b1085e
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-05-09 15:11:16 +0200

    grid working as intended

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,20 @@
 import styles from '../styles/_page.module.scss'
+import PresentationSection from '@/components/presentationSection/presentationSection'
 
 export default function Home() {
   return (
     <div className={styles.grid}>
-      <div className={styles.grid__item}>
-        <h2>Présentation</h2>
-      </div>
-      <div className={styles.grid__item}>
+      <PresentationSection
+        className={`${styles.grid__item} ${styles.presentationSection}`}
+      />
+
+      <div className={`${styles.grid__item} ${styles.competenceSection}`}>
         <h2>Compétences</h2>
       </div>
-      <div className={styles.grid__item}>
+      <div className={`${styles.grid__item} ${styles.socialSection}`}>
         <h2>Réseaux Sociaux</h2>
       </div>
-      <div className={styles.grid__item}>
+      <div className={`${styles.grid__item} ${styles.realisationsSection}`}>
         <h2>Réalisations</h2>
       </div>
       <div className={styles.grid__item}>
---
+++ src/components/presentationSection/presentationSection.js
@@ -0,0 +1,9 @@
+import styles from './_presentationSection.module.scss'
+
+export default function PresentationSection({ className }) {
+  return (
+    <div className={className}>
+      <h2>Présentation</h2>
+    </div>
+  )
+}
